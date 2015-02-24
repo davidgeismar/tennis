1d6858a33afe35cc4cf41f17fb7fecef859521ae
David Geismar <davidgeismar@hotmail.fr>
2015-02-24 10:57:36 +0100

    associations btw models

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,3 +1,4 @@
 class Tournament < ActiveRecord::Base
   belongs_to :user
+  has_many :subscriptions, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :subscriptions, dependent: :destroy
+  has_many :tournaments, dependent: :destroy
 end
