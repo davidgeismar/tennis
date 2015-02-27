4c015374853da95e8aca7409b6d6ef30f7501f74
sacha sardo <sacha.sardo@hotmail.fr>
2015-02-27 11:45:06 +0100

    mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  after_create :send_welcome_email
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   extend Enumerize
@@ -13,21 +14,27 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true, on: :update
   validates :last_name, presence: true, on: :update
 
-
   def self.find_for_facebook_oauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name
-      user.picture = auth.info.image
-      user.token = auth.credentials.token
-      user.token_expiry = Time.at(auth.credentials.expires_at)
-    end
-  end
+   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+     user.provider = auth.provider
+     user.uid = auth.uid
+     user.email = auth.info.email
+     user.password = Devise.friendly_token[0,20]
+     user.first_name = auth.info.first_name
+     user.last_name = auth.info.last_name
+     user.picture = auth.info.image
+     user.token = auth.credentials.token
+     user.token_expiry = Time.at(auth.credentials.expires_at)
+   end
+ end
 
-  def name
-    "#{first_name.capitalize} #{last_name.capitalize}"
+  # def name
+  #   "#{first_name.capitalize} #{last_name.capitalize}"
+  # end
+
+  private
+
+  def send_welcome_email
+    UserMailer.welcome(self).deliver
   end
 end
