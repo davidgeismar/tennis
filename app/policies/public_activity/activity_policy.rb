f586f48ce26897e3b1438500d30491a3071a5ca3
sacha sardo <sacha.sardo@hotmail.fr>
2015-03-03 18:48:02 +0100

    notif

--- app/policies/public_activity/activity_policy.rb
+++ app/policies/public_activity/activity_policy.rb
@@ -1,7 +1,11 @@
-class ActivityPolicy < ApplicationPolicy
+class PublicActivity::ActivityPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       scope.where(recipient: user)
     end
+  end
+
+  def index?
+    true
   end
 end
