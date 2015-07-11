58a26d8c6e9cbf839f34189c28660aa3b14cfa27
David Geismar <davidgeismar@hotmail.fr>
2015-07-11 16:38:03 +0200

    Rename trello credentials

--- config/initializers/trello.rb
+++ config/initializers/trello.rb
@@ -1,6 +1,6 @@
 require 'trello'
 
 Trello.configure do |config|
-  config.developer_public_key = ENV['PUBLIC_TRELLO_KEY']
-  config.member_token = ENV['MEMBER_TRELLO_TOKEN']
+  config.developer_public_key = ENV['TRELLO_PUBLIC_KEY']
+  config.member_token         = ENV['TRELLO_MEMBER_TOKEN']
 end
