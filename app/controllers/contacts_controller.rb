07ce6e1aa9d17412ea91362b55761d894219b5fe
David Geismar <davidgeismar@hotmail.fr>
2015-07-24 17:00:34 +0200

    changed asset host for emails

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,19 +11,19 @@ class ContactsController < ApplicationController
       flash[:alert] = "Votre message a bien été envoyé à l'équipe WeTennis, nous vous contacterons dans les plus brefs délais !"
 
 
-        me = Trello::Member.find("david_geismar")
+        # me = Trello::Member.find("david_geismar")
 
-      # find first board
-      board = me.boards.first
-      puts board.name
-      # puts "Lists: #{board.lists.map {|x| x.name}.join(', ')}"
-      # puts "Members: #{board.members.map {|x| x.full_name}.join(', ')}"
-      board.cards.each do |card|
-            puts "fu"
+      # # find first board
+      # board = me.boards.first
+      # puts board.name
+      # # puts "Lists: #{board.lists.map {|x| x.name}.join(', ')}"
+      # # puts "Members: #{board.members.map {|x| x.full_name}.join(', ')}"
+      # board.cards.each do |card|
+      #       puts "fu"
             # puts "-- Actions: #{card.actions.nil? ? 0 : card.actions.count}"
             # puts "-- Members: #{card.members.count}"
             # puts "-- Labels: #{card.labels.count}"
-      end
+      # end
     else
       render 'new'
     end
