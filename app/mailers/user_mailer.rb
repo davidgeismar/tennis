a7f491a8bea09ac2bb3b2568d0a9ba37a1fb6ac4
David Leuliette <dleuliette@gmail.com>
2015-07-27 23:15:12 +0200

    Typo facutre

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,14 +5,14 @@ class UserMailer < ActionMailer::Base
   def welcome(user)
     @user = user  # Instance variable => available in view
 
-    mail(to: @user.email, subject: 'Bienvenu sur wetennis.fr')
+    mail(to: @user.email, subject: 'Bienvenue sur wetennis.fr')
     # This will render a view in `app/views/user_mailer`!
   end
 
   def welcome_judge(user)
     @user = user  # Instance variable => available in view
 
-    mail(to: @user.email, subject: 'Bienvenu sur wetennis.fr')
+    mail(to: @user.email, subject: 'Bienvenue sur wetennis.fr')
     # This will render a view in `app/views/user_mailer`!
   end
 end
