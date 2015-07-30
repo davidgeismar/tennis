e10d1c052dfecdc1fe433ad8ddbdf77a12afe99f
Cecile Veneziani <contact@cecilitse.org>
2015-07-31 01:19:39 +0200

    Fix facutre in MangoPay env variables

--- config/initializers/mangopay.rb
+++ config/initializers/mangopay.rb
@@ -1,5 +1,5 @@
 MangoPay.configure do |config|
-  config.preproduction      = (ENV['MANGOPAY_RODUCTION'] != true)
+  config.preproduction      = (ENV['MANGOPAY_PRODUCTION'] != "true")
   config.client_id          = ENV['MANGOPAY_CLIENT_ID']
   config.client_passphrase  = ENV['MANGOPAY_CLIENT_PASSPHRASE']
 end
