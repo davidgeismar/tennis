84e24d3eb0eaa7807e4a94875da808dd86c96f7b
Cecile Veneziani <contact@cecilitse.org>
2015-08-02 23:47:29 +0200

    Try to debug omniauth session

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,12 +2,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
 
-
     if user.persisted?
       sign_in_and_redirect user, event: :authentication
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
-      # raise user.errors.full_messages.inspect
+      raise "#{user.errors.full_messages.inspect} --- #{request.env["omniauth.auth"].inspect}"
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
