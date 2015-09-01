71aab1752448ad4b9ba7f3a3dd99058feeafd13b
David Geismar <davidgeismar@hotmail.fr>
2015-09-01 10:41:28 +0200

    didnt find optimal solution to deal with errors in player invitation

--- app/controllers/player_invitations_controller.rb
+++ app/controllers/player_invitations_controller.rb
@@ -5,21 +5,39 @@ class PlayerInvitationsController < ApplicationController
   end
 
   def create
-    @user = User.invite!(email: params[:email], name: "#{params[:first_name]} #{params[:last_name]}")
-
-    @user.first_name      = params[:first_name]
-    @user.last_name       = params[:last_name]
-    @user.licence_number  = params[:licence_number]
-    @user.save
-
-    @subscription = Subscription.new(user: @user, tournament: @tournament)
-
-    if @subscription.save
-      SubscriptionMailer.confirmation_invited_user(@subscription).deliver
-      redirect_to tournament_subscriptions_path(@tournament)
-    else
+    if params[:first_name] == ""
+      flash[:alert] = "Merci de préciser un prénom"
       render :new
+    elsif params[:last_name] == ""
+      flash[:alert] = "Merci de préciser un nom"
+      render :new
+    elsif params[:licence_number] == ""
+
+      flash[:alert] = "Merci de préciser un numéro de licence valide"
+      render :new
+    elsif params[:email] == ""
     end
+      # email must be present in the parameter hash if it is not invitation is not sent
+      @user = User.invite!(email: params[:email], name: "#{params[:first_name]} #{params[:last_name]}")
+      @user.first_name      = params[:first_name]
+      @user.last_name       = params[:last_name]
+      @user.licence_number  = params[:licence_number]
+      @user.save
+
+
+      @subscription = Subscription.new(user: @user, tournament: @tournament)
+      if @subscription.save
+        SubscriptionMailer.confirmation_invited_user(@subscription).deliver
+        redirect_to tournament_subscriptions_path(@tournament)
+      else
+
+        @errors = []
+        @user.errors.messages.each do |key, value|
+          error = key.to_s + ' ' + value.join
+          @errors << error
+        end
+        render :new
+      end
   end
 
   private
