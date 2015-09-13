34cc3db2e75bfd6bc0b5def0691983167d6b8834
David Geismar <davidgeismar@hotmail.fr>
2015-09-13 22:29:34 +0200

    reset password skips user model validations

---
+++ app/assets/javascripts/app/search.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  var delay = (function(){
+    var timer = 0;
+    return function(callback, ms){
+      clearTimeout (timer);
+      timer = setTimeout(callback, ms);
+    };
+  })();
+
+  $('#search_input').keyup(function() {
+      delay(function(){
+        var content = $('#search_input').val();
+       $.get( "tournaments/search/" + content , function( data ) {
+          $( ".result" ).html( data );
+          alert( "Load was performed." );
+      });
+      }, 500 );
+  });
+});
--- app/models/user.rb
+++ app/models/user.rb
@@ -114,6 +114,20 @@ class User < ActiveRecord::Base
     end
   end
 
+  def reset_password!(new_password, new_password_confirmation)
+    self.password = new_password
+    self.password_confirmation = new_password_confirmation
+
+    validates_presence_of     :password
+    validates_confirmation_of :password
+    validates_length_of       :password, within: Devise.password_length, allow_blank: true
+
+    if errors.empty?
+      clear_reset_password_token
+      after_password_reset
+      save(validate: false)
+    end
+  end
 
   private
 
