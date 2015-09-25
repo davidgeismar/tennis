454d9b5f1f7d0d9360a796a5ed61819a8954d584
David Geismar <davidgeismar@hotmail.fr>
2015-09-25 10:43:13 +0200

    fixed player invitation mailer when user has already a wetennis accoutn

--- app/mailers/player_invitations_mailer.rb
+++ app/mailers/player_invitations_mailer.rb
@@ -6,7 +6,7 @@ class PlayerInvitationsMailer < ApplicationMailer
   #   en.convocation_mailer.convocation.subject
   #
   def send_invitation(competition, user_email)
-    @competition = tournament
+    @competition = competition
     @user_email = user_email
 
     mail(to: @user_email, subject: "Vous avez été invité à vous inscrire au tournoi #{@competition.tournament.name}")
