3c6a5e275946027c80a8fc4daff505f118399c81
Cecile Veneziani <contact@cecilitse.org>
2015-10-06 01:21:03 +0200

    [hysteria] Some code cleaning on JS side
    - Alignments
    - Indentation
    - JS file names (snake case!)

--- app/assets/javascripts/app/search.js
+++ app/assets/javascripts/app/search.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   var delay = (function(){
     var timer = 0;
+
     return function(callback, ms){
       clearTimeout (timer);
       timer = setTimeout(callback, ms);
@@ -8,12 +9,12 @@ $(document).ready(function() {
   })();
 
   $('#search_input').keyup(function() {
-      delay(function(){
-        var content = $('#search_input').val();
+    delay(function(){
+      var content = $('#search_input').val();
        $.get( "tournaments/search/" + content , function( data ) {
           $( ".result" ).html( data );
           alert( "Load was performed." );
       });
-      }, 500 );
+    }, 500);
   });
 });
